6bde9cd7cadf2eda210c802fdaeb422f22682f2d
ds423 <aashishranjan05112005@gmail.com>
2026-06-04 13:21:41 +0530

    Add files via upload
    DAY-9

---
+++ Day_9/Q35.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    for(char i='A';i<='E';i++){
+        for(char j='A';j<=i;j++)
+            printf("%c",i);
+
+        printf("\n");
+    }
+
+    return 0;
+}
